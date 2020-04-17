432a2b2d83628707df5c8699ae68ec642b243884
FerretsNook <yarnferret@gmail.com>
2020-04-17 16:11:27 +0200

    Updates

---
+++ precache-manifest.07d44574a15c209276bb583e6203d6c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b11989a146d3981beb47022a77be71c",
+    "url": "/nerdfriends/index.html"
+  },
+  {
+    "revision": "adce65f94883cef953b0",
+    "url": "/nerdfriends/static/css/main.282f99e6.chunk.css"
+  },
+  {
+    "revision": "7e00d832428b3d851469",
+    "url": "/nerdfriends/static/js/2.89467edf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nerdfriends/static/js/2.89467edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adce65f94883cef953b0",
+    "url": "/nerdfriends/static/js/main.8a74c4ec.chunk.js"
+  },
+  {
+    "revision": "28993b0bf61b37a65081",
+    "url": "/nerdfriends/static/js/runtime-main.7908eddf.js"
+  }
+]);
